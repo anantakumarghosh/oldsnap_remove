728db5caed7691594626f83344d76ba89ba34570
Ananta Kumar Ghosh <antaghosh@gmail.com>
2022-08-07 10:37:18 +0530

    Create oldsnap_remove.sh

---
+++ oldsnap_remove.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#close all snaps and run with sudo privileges
+set -eu
+LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
